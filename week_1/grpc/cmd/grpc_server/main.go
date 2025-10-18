d9d9f2a369cf3abd703375e711475bdd98098b43
Radik <Radik.app@yandex.ru>
2025-10-18 15:28:27 +0300

    [week_2] - написал config с разделением на prod и local

--- week_1/grpc/cmd/grpc_server/main.go
+++ week_1/grpc/cmd/grpc_server/main.go
@@ -2,22 +2,27 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
 	"github.com/brianvoe/gofakeit"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
 	"net"
+	"week_1/grpc/internal/config"
 	desc "week_1/grpc/pkg/note_v1"
 )
 
-const (
-	grpcPort = 50051
-)
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "prod.env", "path to config file")
+}
 
 type server struct {
 	desc.UnimplementedNoteV1Server
+	pool *pgxpool.Pool
 }
 
 func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
@@ -39,14 +44,40 @@ func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetRespon
 }
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	flag.Parse()
+	ctx := context.Background()
+
+	// Считываем переменные окружения
+	err := config.Load(configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	grpcConfig, err := config.NewGRPCConfig()
+	if err != nil {
+		log.Fatalf("failed to get grpc config: %v", err)
+	}
+
+	pgConfig, err := config.NewPGConfig()
+	if err != nil {
+		log.Fatalf("failed to get pg config: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", grpcConfig.GRPCAddress())
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	// Создаем пул соединений с базой данных
+	pool, err := pgxpool.Connect(ctx, pgConfig.DSN())
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer pool.Close()
+
 	s := grpc.NewServer()
 	reflection.Register(s)
-	desc.RegisterNoteV1Server(s, &server{})
+	desc.RegisterNoteV1Server(s, &server{pool: pool})
 
 	log.Printf("server listening at %v", lis.Addr())
 
